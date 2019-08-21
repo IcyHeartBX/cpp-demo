55f1eecb132827d7a249f96262fb459802e0f557
pix <tpxsky@163.com>
2019-08-21 14:49:05 +0800

    增加rapid json测试

---
+++ TestRapidJson/TestRapidJson.cpp
@@ -0,0 +1,37 @@
+//============================================================================
+// Name        : TestRapidJson.cpp
+// Author      : pix
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+
+using namespace rapidjson;
+
+int main(void) {
+	puts("Hello World!!!");
+
+	// 1. 把 JSON 解析至 DOM。
+	    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+	    Document d;
+	    d.Parse(json);
+	    // 2. 利用 DOM 作出修改。
+	    Value& s = d["stars"];
+	    s.SetInt(s.GetInt() + 1);
+	    // 3. 把 DOM 转换（stringify）成 JSON。
+	    StringBuffer buffer;
+	    Writer<StringBuffer> writer(buffer);
+	    d.Accept(writer);
+	    // Output {"project":"rapidjson","stars":11}
+	    std::cout << buffer.GetString() << std::endl;
+
+
+	return EXIT_SUCCESS;
+}
